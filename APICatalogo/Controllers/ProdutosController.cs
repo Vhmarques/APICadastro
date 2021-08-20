1f2aa0c776c1c43bb1732df4bc0b18d34ed6bd96
Victor Marques <vhm.marques@hotmail.com>
2021-08-19 23:00:44 -0300

    Adicionado os metodos get de produtos

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using APICatalogo.Context;
 using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
@@ -17,13 +18,26 @@ namespace APICatalogo.Controllers
         public ProdutosController(CatalogoDbContext context)
         {
             _context = context;
-                
+
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            return _context.Produtos.ToList();
+            return _context.Produtos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking()
+                .FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
         }
     }
 }
