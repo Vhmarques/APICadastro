c35e544a9c746956f737fbf979e5cfce1686190d
Victor Marques <vhm.marques@hotmail.com>
2021-08-28 01:15:09 -0300

    Implementando o padrão repository

---
+++ APICatalogo/Repository/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace APICatalogo.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(CatalogoDbContext context) : base(context)
+        {                
+        }
+        public IEnumerable<Categoria> GetCategoriasProdutos()
+        {
+            return Get().Include(c => c.Produtos);
+        }       
+    }
+}
---
+++ APICatalogo/Repository/ProdutoRepository.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APICatalogo.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(CatalogoDbContext context) : base(context)
+        {
+        }
+        public IEnumerable<Produto> GetProdutoPorPreco()
+        {
+            return Get().OrderBy(p => p.Preco).ToList();
+        }
+    }
+}
